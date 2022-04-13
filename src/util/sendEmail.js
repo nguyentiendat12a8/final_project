d710535492161dbf1689f2c4c2aab8e0f3704dd0
nguyentiendat12a8 <74580065+nguyentiendat12a8@users.noreply.github.com>
2022-04-13 11:25:36 +0700

    update mail

--- src/util/sendEmail.js
+++ src/util/sendEmail.js
@@ -3,10 +3,10 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (email, subject, text) => {
     try {
         const transporter = nodemailer.createTransport({
-            host: 'smtp.ethereal.email',
-            service: 'gmail',
-            port: 587,
-            secure: false,
+            //host: 'smtp.ethereal.email',
+            service:"gmail",
+            //port: 587,
+            //secure: false,
             auth: {
                 user: process.env.USER,
                 pass: process.env.PASS,
